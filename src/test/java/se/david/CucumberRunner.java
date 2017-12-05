2afb26cbb6f37c855209ec4918620b7c55b08f11
David Eriksson <davideriksson@swedenmail.com>
2017-12-05 20:37:55 +0100

    Simple cucumber/maven/pitest combination

---
+++ src/test/java/se/david/CucumberRunner.java
@@ -0,0 +1,10 @@
+package se.david;
+
+import cucumber.api.CucumberOptions;
+import cucumber.api.junit.Cucumber;
+import org.junit.runner.RunWith;
+
+@RunWith(Cucumber.class)
+@CucumberOptions(features = "classpath:features/date_calculator.feature")
+public class CucumberRunner {
+}
